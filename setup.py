fa58afb17755eb0dc9a2b3e8863015150266d68b
kakusyun <45459635+kakusyun@users.noreply.github.com>
2021-04-20 22:11:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(name="qpnet-pytorch",
       packages=find_packages(exclude=['.txt', '.jpg', '.jpeg', '.bmp',
                                       '.pkl', '.xml', '.xlsx', '.csv', '.png',
